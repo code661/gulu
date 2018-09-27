17ebc5317c366c41ae6e992c86fbdc0c3bc3b8c1
Ryan <lucky.tanduzhi@gmail.com>
2018-09-28 07:48:04 +0800

    添加 icon 和orientation 属性

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,31 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${orientation}`]:true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+
+  </button>
+  </button>
 </template>
 
 <script>
-  export default {};
+export default {
+  props:{
+    icon: String,
+    orientation: String
+  }
+};
 </script>
 
 <style lang="scss">
 .g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   font-size: var(--font-size);
   height: var(--button-height);
   border-radius: var(--border-radius);
@@ -23,5 +41,26 @@
   &:focus {
     outline: none;
   }
+  > .icon {
+    order: 1;
+    margin-right: .3em
+  }
+  > .content {
+    order: 2
+  }
+  &.icon-right {
+    > .icon{
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em
+    }
+    > .content {
+      order: 1;
+    }
+  }
+}
+
+.i-right:last-child {
+  order: 1
 }
 </style>
