2ff05bfde5f8c52cfa01b2446161ec9684053ac2
Ryan <lucky.tanduzhi@gmail.com>
2018-09-29 10:11:10 +0800

    完成 g-button-group 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import Button from './button'
 import Icon from "./Icon"
+import buttonGroup from "./buttonGroup"
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group',buttonGroup)
+
 new Vue({
   el: '#app',
   data:{
---
+++ src/buttonGroup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "g-button-grounp",
+  mounted(){
+    for(let node of this.$el.children){
+      if(node.nodeName.toLowerCase() !== "button"){
+        console.error(`g-button-group 组件的子组件应该是g-button`)
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+.g-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button{
+    border-radius: 0;
+    &:not(:first-child){
+      margin-left: -1px;
+    }
+    &:first-child{
+      border-radius: var(--border-radius) 0 0 var(--border-radius)
+    }
+    &:last-child{
+      border-radius: 0 var(--border-radius) var(--border-radius) 0
+    }
+    &:hover{
+      z-index: 1
+    }
+  }
+}
+</style>
+
+
+
