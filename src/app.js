c482f0d29b4d23c9af6b16a7dc2e99ba888b5f27
Ryan <lucky.tanduzhi@gmail.com>
2018-09-29 18:08:06 +0800

    添加 3个测试用例

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Button from './button'
 import Icon from "./Icon"
 import buttonGroup from "./buttonGroup"
+import chai from "chai"
+let expect = chai.expect
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -14,3 +16,51 @@ new Vue({
     loading2: false
   },
 })
+
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon: "settings"
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute("xlink:href")
+  expect(href).to.equal("#i-settings")
+  button.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      loading: true,
+      icon: "settings"
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute("xlink:href")
+  expect(href).to.equal("#i-loading")
+  button.$destroy()
+}
+
+{
+  let div = document.createElement("div")
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon: "settings",
+      orientation: "right"
+    }
+  })
+  button.$mount(div)
+  window.test = button.$el
+  let svg = button.$el.querySelector('svg')
+  let order = window.getComputedStyle(svg).order
+  expect(order).to.equal("2")
+  button.$el.remove()
+  button.$destroy()
+}
