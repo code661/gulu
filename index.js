f2f865a21ea18b5da19c9da1053592eae323c8e0
Ryan <lucky.tanduzhi@gmail.com>
2018-10-01 09:05:25 +0800

    导出 input 组件

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from "./src/Button"
 import ButtonGroup from "./src/ButtonGroup"
 import Icon from "./src/Icon"
+import Input from "./src/Input"
 
-export {Button, ButtonGroup, Icon}
+export {Button, ButtonGroup, Icon, Input}
