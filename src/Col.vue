91cb25b7a2b8c1549e7f5413e95abafe33b90887
Ryan <lucky.tanduzhi@gmail.com>
2018-10-03 14:12:44 +0800

    实现响应式布局

--- src/Col.vue
+++ src/Col.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="col"
-    :class="[
-      span && `col-${span}`,
-      offset && `offset-${offset}`,
-    ]"
+    :class="colClasses"
     :style="colStyle"
   >
     <slot></slot>
@@ -11,6 +8,17 @@
 </template>
 
 <script>
+let validator = value => {
+  let keys = Object.keys(value);
+  let valid = true;
+  keys.forEach(key => {
+    if (!["span", "offset"].includes(key)) {
+      valid = false;
+    }
+  });
+  return valid;
+};
+
 export default {
   data() {
     return {
@@ -23,15 +31,32 @@ export default {
     },
     offset: {
       type: [String, Number]
-    }
+    },
+    pad: { type: Object, validator },
+    narrowPc: { type: Object, validator },
+    pc: { type: Object, validator },
+    widePc: { type: Object, validator },
+    ultraWidePc: { type: Object, validator }
   },
   computed: {
     colStyle() {
-      let {gutter} = this
+      let { gutter } = this;
       return {
         paddingLeft: gutter / 2 + "px",
         paddingRight: gutter / 2 + "px"
       };
+    },
+    colClasses() {
+      let { span, offset, pad, narrowPc, pc, widePc, ultraWidePc } = this;
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+        ...(pad ? [`pad-col-${pad.span}`] : []),
+        ...(narrowPc ? [`narrowPc-col-${narrowPc.span}`] : []),
+        ...(pc ? [`pc-col-${pc.span}`] : []),
+        ...(widePc ? [`widePc-col-${widePc.span}`] : []),
+        ...(ultraWidePc ? [`ultraWidePc-col-${ultraWidePc.span}`] : [])
+      ];
     }
   }
 };
@@ -50,6 +75,85 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: ($n / 24) * 100%;
+    }
+  }
+  @media (min-width: 576px) {
+    $class-prefix: pad-col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: pad-offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    $class-prefix: narrowPc-col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: narrowPc-offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 992px) {
+    $class-prefix: pc-col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: pc-offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 1200px) {
+    $class-prefix: widePc-col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: widePc-offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 1600px) {
+    $class-prefix: ultraWidePc-col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: ultraWidePc-offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
     }
   }
 }
