3b4552d9b3c46433db40a3932ddb9ec313fdca89
Ryan <lucky.tanduzhi@gmail.com>
2018-10-06 03:05:59 +0800

    完成 Toast 基础样式

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="toast">
-    <slot></slot>
-    <span v-if="closeButton" @click="close">{{closeButton.text}}</span>
+    <div class="message-wrap">
+      <div v-if="enableHtml" v-html="$slots.default[0]"></div>
+      <slot v-else></slot>
+    </div>
+    <div class="close-wrap" v-if="!autoClose" >
+      <hr>
+      <span class="close-tip" @click="clickCloseBtton">{{closeButton.text}}</span>
+    </div>
   </div>  
 </template>
 
@@ -24,28 +30,76 @@ export default {
           callBack: null
         };
       }
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
+    clickCloseBtton() {
+      this.close();
+      if (this.closeButton && typeof this.closeButton.callBack === "function") {
+        this.closeButton.callBack(this);
+      }
+    },
     close() {
       this.$el.remove();
       this.$destroy();
-      if (this.closeButton && typeof this.closeButton.callBack === "function") {
-        this.closeButton.callBack(this);
+    },
+    setAutoCloseTimer() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
       }
     }
   },
   mounted() {
-    if (this.autoClose) {
-      setTimeout(() => {
-        this.close();
-      }, this.autoCloseDelay * 1000);
-    }
+    this.setAutoCloseTimer();
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.toast {
+  $toast-bg: rgba(0, 0, 0, 0.74);
+  $toast-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
+  $toast-color: white;
+  $toast-font-size: 14px;
+  $toast-border-radius: 4px;
+  
+  background: $toast-bg;
+  color: $toast-color;
+  border-radius: $toast-border-radius;
+  box-shadow: $toast-box-shadow;
+  font-size: $toast-font-size;
+  display: flex;
+  position: fixed;
+  top: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  .message-wrap {
+    max-width: 250px;
+    padding: 8px 16px;
+  }
+  .close-wrap {
+    flex-shrink: 0;
+    display: flex;
+    hr {
+      border: none;
+      width: 1px;
+      background-color: #666666;
+      transform: scaleX(0.4)
+    }
+    .close-tip {
+      display: flex;
+      padding: 0 16px;
+      align-items: center;
+      cursor: pointer;
+    }
+  }
+}
 </style>
 
 
