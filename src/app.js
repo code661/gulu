717088d48eaea164a396f1d20ea651cc779e2ba8
Ryan <lucky.tanduzhi@gmail.com>
2018-10-05 08:10:57 +0800

    完成 Layout 组件

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import buttonGroup from "./buttonGroup";
 import Input from "./Input"
 import Col from "./Col"
 import Row from "./Row"
+import Layout from "./Layout"
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+import Sider from "./Sider"
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
@@ -12,6 +17,12 @@ Vue.component("g-button-group", buttonGroup);
 Vue.component("g-input", Input);
 Vue.component("g-row", Row);
 Vue.component("g-col", Col);
+Vue.component("g-layout", Layout);
+Vue.component("g-header", Header);
+Vue.component("g-content", Content);
+Vue.component("g-footer", Footer);
+Vue.component("g-sider", Sider);
+
 
 new Vue({
   el: "#app",
