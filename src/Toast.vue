97d1d4e97de05def184cf7bb79104a64cdae3d97
Ryan <lucky.tanduzhi@gmail.com>
2018-10-06 08:10:29 +0800

    完成 Toast 组件

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,25 +1,24 @@
 <template>
-  <div class="toast">
-    <div class="message-wrap">
-      <div v-if="enableHtml" v-html="$slots.default[0]"></div>
-      <slot v-else></slot>
-    </div>
-    <div class="close-wrap" v-if="!autoClose" >
-      <hr>
-      <span class="close-tip" @click="clickCloseBtton">{{closeButton.text}}</span>
-    </div>
-  </div>  
+  <div class="toast-wrap" :class="toastClass">
+    <div class="toast">
+      <div class="message-wrap">
+        <div v-if="enableHtml" v-html="$slots.default[0]"></div>
+        <slot v-else></slot>
+      </div>
+      <div class="close-wrap" v-if="!autoClose || clickCloseBtton" >
+        <hr>
+        <span class="close-tip" @click="clickCloseBtton">{{closeButton.text || "关闭"}}</span>
+      </div>
+    </div>  
+  </div>
+
 </template>
 
 <script>
 export default {
   props: {
     autoClose: {
-      type: Boolean,
-      default: true
-    },
-    autoCloseDelay: {
-      type: Number,
+      type: [Number, Boolean],
       default: 5
     },
     closeButton: {
@@ -34,7 +33,17 @@ export default {
     enableHtml: {
       type: Boolean,
       default: false
+    },
+    toastPosition: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "middle", "bottom"].indexOf(value) >= 0;
+      }
     }
+  },
+  mounted() {
+    this.setAutoCloseTimer();
   },
   methods: {
     clickCloseBtton() {
@@ -45,61 +54,114 @@ export default {
     },
     close() {
       this.$el.remove();
+      this.$emit("close");
       this.$destroy();
     },
     setAutoCloseTimer() {
-      if (this.autoClose) {
+      if (this.autoClose === true) {
         setTimeout(() => {
           this.close();
-        }, this.autoCloseDelay * 1000);
+        }, 5000);
+      } else if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoClose * 1000);
       }
     }
   },
-  mounted() {
-    this.setAutoCloseTimer();
+  computed: {
+    toastClass() {
+      return { [`position-${this.toastPosition}`]: true };
+    }
   }
 };
 </script>
-
 <style lang="scss" scoped>
-.toast {
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes slide-down {
+  0% {
+    transform: translateY(-100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-up {
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+.toast-wrap {
   $toast-bg: rgba(0, 0, 0, 0.74);
   $toast-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
   $toast-color: white;
   $toast-font-size: 14px;
   $toast-border-radius: 4px;
-  
-  background: $toast-bg;
-  color: $toast-color;
-  border-radius: $toast-border-radius;
-  box-shadow: $toast-box-shadow;
-  font-size: $toast-font-size;
-  display: flex;
   position: fixed;
-  top: 0px;
   left: 50%;
   transform: translateX(-50%);
-  .message-wrap {
-    max-width: 250px;
-    padding: 8px 16px;
-  }
-  .close-wrap {
-    flex-shrink: 0;
-    display: flex;
-    hr {
-      border: none;
-      width: 1px;
-      background-color: #666666;
-      transform: scaleX(0.4)
+  &.position-top {
+    top: 0;
+    .toast {
+      border-top-left-radius: 0px;
+      border-top-right-radius: 0px;
+      animation: slide-down 0.4s linear;
     }
-    .close-tip {
+  }
+  &.position-middle {
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    .toast {
+      animation: fade-in 0.4s linear;
+    }
+  }
+  &.position-bottom {
+    bottom: 0;
+    .toast {
+      border-bottom-left-radius: 0px;
+      border-bottom-right-radius: 0px;
+      animation: slide-up 0.4s linear;
+    }
+  }
+  .toast {
+    background: $toast-bg;
+    color: $toast-color;
+    border-radius: $toast-border-radius;
+    box-shadow: $toast-box-shadow;
+    font-size: $toast-font-size;
+    display: flex;
+    .message-wrap {
+      max-width: 250px;
+      padding: 8px 16px;
+    }
+    .close-wrap {
+      flex-shrink: 0;
       display: flex;
-      padding: 0 16px;
-      align-items: center;
-      cursor: pointer;
+      hr {
+        border: none;
+        width: 1px;
+        background-color: #666666;
+        transform: scaleX(0.4);
+      }
+      .close-tip {
+        display: flex;
+        padding: 0 16px;
+        align-items: center;
+        cursor: pointer;
+      }
     }
   }
 }
 </style>
-
-
--- src/plugin.js
+++ src/plugin.js
@@ -1,19 +1,21 @@
 import Toast from "./Toast";
-
+let currentToast = null;
 export default {
   install(Vue, options) {
-    Vue.prototype.$toast = (message, { autoClose, autoCloseDelay, closeButton }) => {
-      let Constructor = Vue.extend(Toast);
-      let toast = new Constructor({
-        propsData: {
-          autoClose,
-          autoCloseDelay,
-          closeButton
-        }
-      });
-      toast.$slots.default = [message];
-      toast.$mount();
-      document.body.appendChild(toast.$el);
+    Vue.prototype.$toast = (message, toastOptions) => {
+      currentToast && currentToast.close()
+      currentToast = createToast(Vue, message, toastOptions);
     };
   }
+};
+let createToast = (Vue, message, toastOptions) => {
+  let Constructor = Vue.extend(Toast);
+  let toast = new Constructor({ propsData: toastOptions });
+  toast.$slots.default = [message];
+  toast.$on("close", () => {
+    currentToast = null;
+  });
+  toast.$mount();
+  document.body.appendChild(toast.$el);
+  return toast;
 };
