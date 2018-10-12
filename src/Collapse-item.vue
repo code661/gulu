e4b57ccca71560f0667a5821b641efa52aff0c05
Ryan <lucky.tanduzhi@gmail.com>
2018-10-13 06:43:54 +0800

    完成基本样式和功能

---
+++ src/Collapse-item.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="collapse-item" @click="open = !open">
+    <div class="title">{{title}}</div>
+    <div class="content" v-show="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from "./Icon";
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data(){
+    return{
+      open: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.collapse-item {
+  $border-color: #dcdee2;
+  &:not(:first-child) {
+    border-top: 1px solid $border-color;
+  }
+  > .title {
+    padding-left: 12px;
+    line-height: 30px;
+    
+  }
+  > .content {
+    border-top: 1px solid $border-color;
+    background-color: white;
+    padding: 12px;
+  }
+  &:last-child {
+    > .content {
+      border-radius: 0 0 3px 3px;
+    }
+  }
+}
+</style>
---
+++ src/Collapse.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  data() {
+    return {
+      eventBus: new Vue()
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.collapse {
+  $backgroung-color: #eeeeee;
+  $border-color: #cccccc;
+  border: 1px solid $border-color;
+  border-radius: 4px;
+  background-color: $backgroung-color;
+  font-size: 13px;
+  min-width: 200px;
+}
+</style>
