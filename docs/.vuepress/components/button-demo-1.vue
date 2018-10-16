3f82ef0d675d649ccb7404031995b78585885add
Ryan <lucky.tanduzhi@gmail.com>
2018-10-16 11:42:44 +0800

    完成 Button 组件的文档

--- docs/.vuepress/components/button-demo-1.vue
+++ docs/.vuepress/components/button-demo-1.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
-    <g-button icon="settings"></g-button>
+    <g-button>默认按钮</g-button>
+    <g-button icon="settings">设置</g-button>
+    <g-button :loading="true">加载中</g-button>
+    <g-button icon="info" 
+      @click="loading=!loading" 
+      :loading="loading"
+    >点击加载</g-button>
   </div>
 </template>
 
 <script>
 import Button from "../../../src/button.vue";
 export default {
+  components: {
+    "g-button": Button
+  },
   data() {
     return {
-      msg: "hello"
+      loading: false
     };
-  },
-  components:{
-    "g-button": Button
   }
 };
 </script>
